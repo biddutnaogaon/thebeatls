53c2b204e2beab389ef818a4819953ddc0b0e713
Md. Biddut Hossain <79271026+biddutnaogaon@users.noreply.github.com>
2022-05-11 20:16:03 +0600

    Add files via upload

---
+++ beatls/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+     
+    $("#owl-demo").owlCarousel({
+   
+        autoPlay: 2500, //Set AutoPlay to 2.5 seconds
+   
+        items : 3,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [991,2]
+   
+    });
+   
+  });
+
+
+   // Animation on Scroll Initiation
+   AOS.init();
+  
+  
+  
+  
---
+++ beatls/js/scrolltop.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".ScrollToTop").click(function() {
+        $("html, body").animate({ 
+            scrollTop: 0 
+        }, "slow");
+        return false;
+    });
+  });
